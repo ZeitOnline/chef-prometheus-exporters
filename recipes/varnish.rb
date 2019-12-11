e6b25e534d6129a6a149e59c6da860bebc3ba208
Stephan Scheying <stephan@scheying.de>
2019-12-11 08:32:33 +0100

    Add default for port setting

--- recipes/varnish.rb
+++ recipes/varnish.rb
@@ -8,11 +8,12 @@
 #
 
 unless node['prometheus_exporters']['disable']
+  node_port = node['prometheus_exporters']['varnish']['port']
+
   varnish_exporter 'main' do
     varnishstat node['prometheus_exporters']['varnish']['varnishstat']
     user node['prometheus_exporters']['varnish']['user']
-    telemetry_address node['prometheus_exporters']['varnish']['telemetry_address']
-    telemetry_endpoint node['prometheus_exporters']['varnish']['telemetry_endpoint']
+    telemetry_address ":#{node_port}"
 
     action %i[install enable start]
   end
